a466b6df3d8bef266140603a0acc3ea8179e28f2
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-05-25 16:59:20 +0200

    Fix w2g share parameter

--- src/main/java/net/kardexo/ts3bot/commands/impl/CommandWatch2Gether.java
+++ src/main/java/net/kardexo/ts3bot/commands/impl/CommandWatch2Gether.java
@@ -4,6 +4,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.regex.Pattern;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mojang.brigadier.CommandDispatcher;
@@ -52,13 +53,19 @@ public class CommandWatch2Gether
 		
 		try
 		{
+			ObjectMapper mapper = new ObjectMapper();
 			HttpURLConnection connection = (HttpURLConnection) new URL(BASE_URL + "create.json").openConnection();
+			byte[] content = mapper.writeValueAsBytes(new Watch2Gether(TS3Bot.getInstance().getConfig().getApiWatch2Gether(), Util.extractURL(url)));
+			
+			connection.setDoOutput(true);
 			connection.setRequestMethod("POST");
-			connection.addRequestProperty("api_key", TS3Bot.getInstance().getConfig().getApiWatch2Gether());
-			connection.addRequestProperty("share", Util.extractURL(url));
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setRequestProperty("Content-Length", String.valueOf(content.length));
+			
+			connection.getOutputStream().write(content);
 			connection.connect();
 			
-			JsonNode node = new ObjectMapper().readTree(connection.getInputStream());
+			JsonNode node = mapper.readTree(connection.getInputStream());
 			
 			context.getSource().sendFeedback(BASE_URL + node.path("streamkey").asText());
 			return 0;
@@ -67,6 +74,46 @@ public class CommandWatch2Gether
 		{
 			e.printStackTrace();
 			throw WATCH2GETHER_SERVICE_UNAVAILABLE.create();
+		}
+	}
+	
+	public static class Watch2Gether
+	{
+		@JsonProperty("api_key")
+		private String apiKey;
+		
+		@JsonProperty("share")
+		private String share;
+		
+		public Watch2Gether()
+		{
+			super();
+		}
+		
+		public Watch2Gether(String apiKey, String share)
+		{
+			this.apiKey = apiKey;
+			this.share = share;
+		}
+		
+		public String getApiKey()
+		{
+			return this.apiKey;
+		}
+		
+		public void setApiKey(String apiKey)
+		{
+			this.apiKey = apiKey;
+		}
+		
+		public String getShare()
+		{
+			return this.share;
+		}
+		
+		public void setShare(String share)
+		{
+			this.share = share;
 		}
 	}
 }
