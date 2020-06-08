4c76bf91009a8a7c34bdcf5ff2c0dd7f51e35064
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-06-09 00:33:38 +0200

    Use jsoup for title parsing

--- src/main/java/net/kardexo/ts3bot/processors/url/impl/DefaultURLProcessor.java
+++ src/main/java/net/kardexo/ts3bot/processors/url/impl/DefaultURLProcessor.java
@@ -3,16 +3,14 @@ package net.kardexo.ts3bot.processors.url.impl;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.core.util.IOUtils;
+import org.jsoup.Jsoup;
 
 import net.kardexo.ts3bot.processors.url.IURLProcessor;
 
 public class DefaultURLProcessor implements IURLProcessor
 {
-	private static final Pattern TITLE = Pattern.compile(".*<title>([^<>]*)</title>.*");
 	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246";
 	
 	@Override
@@ -25,15 +23,7 @@ public class DefaultURLProcessor implements IURLProcessor
 			connection.setConnectTimeout(5000);
 			connection.connect();
 			
-			if(connection.getContentType() != null && connection.getContentType().startsWith("text/html"))
-			{
-				Matcher matcher = TITLE.matcher(IOUtils.toString(new InputStreamReader(connection.getInputStream())));
-				
-				if(matcher.find())
-				{
-					return matcher.group(1);
-				}
-			}
+			return Jsoup.parse(IOUtils.toString(new InputStreamReader(connection.getInputStream()))).getElementsByTag("title").first().text();
 		}
 		catch(Exception e)
 		{
