7409e1089722fbd5a471a39b45511d2ac4ed24b4
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-06-08 17:20:17 +0200

    Add user agent request property

--- src/main/java/net/kardexo/ts3bot/processors/url/impl/DefaultURLProcessor.java
+++ src/main/java/net/kardexo/ts3bot/processors/url/impl/DefaultURLProcessor.java
@@ -13,6 +13,7 @@ import net.kardexo.ts3bot.processors.url.IURLProcessor;
 public class DefaultURLProcessor implements IURLProcessor
 {
 	private static final Pattern TITLE = Pattern.compile(".*<title>([^<>]*)</title>.*");
+	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246";
 	
 	@Override
 	public String process(String url)
@@ -20,10 +21,11 @@ public class DefaultURLProcessor implements IURLProcessor
 		try
 		{
 			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+			connection.setRequestProperty("User-Agent", USER_AGENT);
 			connection.setConnectTimeout(5000);
 			connection.connect();
 			
-			if(connection.getContentType().startsWith("text/html"))
+			if(connection.getContentType() != null && connection.getContentType().startsWith("text/html"))
 			{
 				Matcher matcher = TITLE.matcher(IOUtils.toString(new InputStreamReader(connection.getInputStream())));
 				
