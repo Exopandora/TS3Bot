15b9adea505291cdc885370ddddb537f286c726e
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-08-09 16:08:08 +0200

    Fix api url

--- src/main/java/net/kardexo/ts3bot/commands/impl/CommandWatch2Gether.java
+++ src/main/java/net/kardexo/ts3bot/commands/impl/CommandWatch2Gether.java
@@ -22,7 +22,7 @@ import net.kardexo.ts3bot.util.URLs;
 
 public class CommandWatch2Gether
 {
-	private static final String API_URL = "https://www.w2g.tv/rooms/";
+	private static final String API_URL = "https://www.watch2gether.com/rooms/";
 	private static final SimpleCommandExceptionType WATCH2GETHER_SERVICE_UNAVAILABLE = new SimpleCommandExceptionType(new LiteralMessage("Watch2Gether is currently unavailable"));
 	
 	public static void register(CommandDispatcher<CommandSource> dispatcher)
