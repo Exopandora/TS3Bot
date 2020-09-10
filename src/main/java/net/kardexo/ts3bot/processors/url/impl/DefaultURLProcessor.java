507ff9521a983c5f4acf418046cfd458cb8d29ca
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-09-10 17:27:48 +0200

    Only accept text/html mime type for title extraction

--- src/main/java/net/kardexo/ts3bot/processors/url/impl/DefaultURLProcessor.java
+++ src/main/java/net/kardexo/ts3bot/processors/url/impl/DefaultURLProcessor.java
@@ -13,6 +13,7 @@ import net.kardexo.ts3bot.processors.url.IURLProcessor;
 public class DefaultURLProcessor implements IURLProcessor
 {
 	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246";
+	private static final String MIME_TYPE_TEXT_HTML = "text/html";
 	
 	@Override
 	public String process(String url)
@@ -21,15 +22,23 @@ public class DefaultURLProcessor implements IURLProcessor
 		{
 			HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
 			connection.setRequestProperty("User-Agent", USER_AGENT);
-			connection.setRequestProperty("Accept-Charset", "UTF-8"); 
+			connection.setRequestProperty("Accept", MIME_TYPE_TEXT_HTML);
+			connection.setRequestProperty("Accept-Charset", "UTF-8");
 			connection.setConnectTimeout(5000);
 			connection.connect();
 			
-			try
+			if(MIME_TYPE_TEXT_HTML.equals(connection.getContentType()))
 			{
-				return Jsoup.parse(IOUtils.toString(new InputStreamReader(connection.getInputStream(), "UTF-8"))).getElementsByTag("title").first().text();
+				try
+				{
+					return Jsoup.parse(IOUtils.toString(new InputStreamReader(connection.getInputStream(), "UTF-8"))).getElementsByTag("title").first().text();
+				}
+				finally
+				{
+					connection.disconnect();
+				}
 			}
-			finally
+			else
 			{
 				connection.disconnect();
 			}
