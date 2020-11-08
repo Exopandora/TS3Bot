509198c3bf0cc80dfbed03d86caa877891e9121b
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-11-08 01:38:43 +0100

    Add kick command

--- src/main/java/net/kardexo/ts3bot/TS3Bot.java
+++ src/main/java/net/kardexo/ts3bot/TS3Bot.java
@@ -29,6 +29,7 @@ import net.kardexo.ts3bot.commands.impl.CommandCoinflip;
 import net.kardexo.ts3bot.commands.impl.CommandExit;
 import net.kardexo.ts3bot.commands.impl.CommandGameServers;
 import net.kardexo.ts3bot.commands.impl.CommandHelp;
+import net.kardexo.ts3bot.commands.impl.CommandKick;
 import net.kardexo.ts3bot.commands.impl.CommandLeagueOfLegends;
 import net.kardexo.ts3bot.commands.impl.CommandMove;
 import net.kardexo.ts3bot.commands.impl.CommandRandom;
@@ -158,6 +159,7 @@ public class TS3Bot extends TS3EventAdapter
 		CommandLeagueOfLegends.register(this.dispatcher);
 		CommandGameServers.register(this.dispatcher);
 		CommandCoinflip.register(this.dispatcher);
+		CommandKick.register(this.dispatcher);
 	}
 	
 	private void registerMessageProcessors()
---
+++ src/main/java/net/kardexo/ts3bot/commands/impl/CommandKick.java
@@ -0,0 +1,49 @@
+package net.kardexo.ts3bot.commands.impl;
+
+import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+
+import net.kardexo.ts3bot.TS3Bot;
+import net.kardexo.ts3bot.commands.CommandSource;
+import net.kardexo.ts3bot.commands.Commands;
+
+public class CommandKick
+{
+	private static final DynamicCommandExceptionType USERNAME_NOT_FOUND = new DynamicCommandExceptionType(username -> new LiteralMessage("Could not find user " + username));
+	
+	public static void register(CommandDispatcher<CommandSource> dispatcher)
+	{
+		dispatcher.register(Commands.literal("kick")
+				.executes(context -> kick(context))
+				.then(Commands.argument("username", StringArgumentType.greedyString())
+						.requires(source -> source.hasPermission("admin"))
+						.executes(context -> kick(context, StringArgumentType.getString(context, "username")))));
+	}
+	
+	private static int kick(CommandContext<CommandSource> context) throws CommandSyntaxException
+	{
+		int clientId = context.getSource().getClientInfo().getId();
+		TS3Bot.getInstance().getApi().kickClientFromServer(clientId);
+		return clientId;
+	}
+	
+	private static int kick(CommandContext<CommandSource> context, String username) throws CommandSyntaxException
+	{
+		for(Client client : TS3Bot.getInstance().getApi().getClients())
+		{
+			if(username.equalsIgnoreCase(client.getNickname()))
+			{
+				int clientId = client.getId();
+				TS3Bot.getInstance().getApi().kickClientFromServer(clientId);
+				return clientId;
+			}
+		}
+		
+		throw USERNAME_NOT_FOUND.create(username);
+	}
+}
