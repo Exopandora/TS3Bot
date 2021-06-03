74304499bfbac2d2101f2872cf385722082da85b
Marcel Konrad <mkonrad98@alice-dsl.net>
2021-06-03 20:52:17 +0200

    Fix operators

--- src/main/java/net/kardexo/ts3bot/commands/impl/CommandCalculate.java
+++ src/main/java/net/kardexo/ts3bot/commands/impl/CommandCalculate.java
@@ -156,6 +156,10 @@ public class CommandCalculate
 						{
 							x %= this.parseFactor();
 						}
+						else
+						{
+							throw new ParseException("Unknown function: " + function, start);
+						}
 					}
 					else
 					{
