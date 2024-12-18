502a7ef620a5b4ebd51475dfb98a5f5833389bde
Exopandora <mkonrad98@outlook.com>
2024-12-18 22:22:19 +0100

    Ignore unknown config keys

--- discord/src/main/java/net/kardexo/bot/adapters/discord/DiscordConfigAdapter.java
+++ discord/src/main/java/net/kardexo/bot/adapters/discord/DiscordConfigAdapter.java
@@ -1,5 +1,6 @@
 package net.kardexo.bot.adapters.discord;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import net.kardexo.bot.domain.config.Config;
 
@@ -8,6 +9,7 @@ import java.io.IOException;
 
 import static net.kardexo.bot.domain.Util.OBJECT_MAPPER;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DiscordConfigAdapter extends Config
 {
 	@JsonProperty("token")
