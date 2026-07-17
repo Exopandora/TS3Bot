da44b404ee81ec03fba8ce4a4843ef9483d203a9
Exopandora <mkonrad98@outlook.com>
2026-07-18 01:05:12 +0200

    Implement test style factory

---
+++ common/src/testFixtures/java/net/kardexo/bot/testfixtures/TestStyle.java
@@ -0,0 +1,51 @@
+package net.kardexo.bot.testfixtures;
+
+import net.kardexo.bot.domain.api.IStyle;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public class TestStyle implements IStyle
+{
+	protected static final TestStyle BOLD = new TestStyle("bold");
+	protected static final TestStyle UNDERLINED = new TestStyle("underlined");
+	protected static final TestStyle ITALIC = new TestStyle("italic");
+	protected static final TestStyle STRIKETHROUGH = new TestStyle("strike");
+	
+	private final String code;
+	
+	protected TestStyle(String code)
+	{
+		this.code = code;
+	}
+	
+	@Override
+	public StringBuilder apply(StringBuilder builder, String string)
+	{
+		builder.append("<");
+		builder.append(this.code);
+		builder.append(">");
+		builder.append(string);
+		builder.append("</");
+		builder.append(this.code);
+		builder.append(">");
+		return builder;
+	}
+	
+	@Override
+	public boolean equals(Object object)
+	{
+		if(!(object instanceof TestStyle testStyle))
+		{
+			return false;
+		}
+		
+		return Objects.equals(this.code, testStyle.code);
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(this.code);
+	}
+}
---
+++ common/src/testFixtures/java/net/kardexo/bot/testfixtures/TestStyleFactory.java
@@ -0,0 +1,43 @@
+package net.kardexo.bot.testfixtures;
+
+import net.kardexo.bot.domain.api.IStyle;
+import net.kardexo.bot.services.api.IStyleFactory;
+
+public class TestStyleFactory implements IStyleFactory
+{
+	@Override
+	public IStyle color(int color)
+	{
+		return new TestStyle(String.format("#%02X%02X%02X", (color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF));
+	}
+	
+	@Override
+	public IStyle bold()
+	{
+		return TestStyle.BOLD;
+	}
+	
+	@Override
+	public IStyle underlined()
+	{
+		return TestStyle.UNDERLINED;
+	}
+	
+	@Override
+	public IStyle italic()
+	{
+		return TestStyle.ITALIC;
+	}
+	
+	@Override
+	public IStyle strikethrough()
+	{
+		return TestStyle.STRIKETHROUGH;
+	}
+	
+	@Override
+	public int colorOverhead()
+	{
+		return 0;
+	}
+}
